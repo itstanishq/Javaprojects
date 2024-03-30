24f6d5ca7f7d36f3ac5124e9c819e5630c7f0579
TANISHQ BHARDWAJ <95243620+itstanishq@users.noreply.github.com>
2024-03-30 12:45:16 +0000

    intro of array

---
+++ arr/intro.java
@@ -0,0 +1,16 @@
+package arr;
+
+public class intro {
+  public static void main(String[] args) {
+    //syntax
+
+    // datatype[] variable_name = new datatype[size];
+
+    // store 5 roll number
+
+    int[] ros; // declaration of array .ros is getting defined in the stack 
+    ros = new int[5]; //actually here object is being created in the memory.
+    
+  }
+
+}
